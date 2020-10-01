dbd8409b578ecc4d41452c47e847c09cef9116f9
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-09-30 18:44:58 -0600

    fixed navbar

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
     <div id="footer">
-      <hr/>
       <p>Lovely Nails Boise ©2020</p>
       <p>208-433-1777</p>
       <p>2721 Broadway Ave. Boise, ID</p>
--- nav.php
+++ nav.php
@@ -6,9 +6,15 @@
     <meta charset="UTF-8">
   </head>
   <body>
-    <img class="logo" alt="lovely-nails-logo" src="images/lovelynailslogo.png">
     <nav>
-        <a <?php if ($pageName == "home") { echo "class='active';"; } ?> href="index.php">Home</a>
-        <a <?php if ($pageName == "about") { echo "class='active';"; } ?> href="about.php">About</a>
-        <a <?php if ($pageName == "services") { echo "class='active';"; } ?> href="services.php">Services</a>
+      <img class="logo" alt="lovely-nails-logo" src="images/lovelynailslogo.png">
+      <a <?php if ($pageName == "home") { echo "class='active';"; } ?> href="index.php">Home</a>
+      <a <?php if ($pageName == "about") { echo "class='active';"; } ?> href="about.php">About</a>
+      <a <?php if ($pageName == "services") { echo "class='active';"; } ?> href="services.php">Services</a>
+      <div class="nav-item">
+        <p>208-433-1777</p>
+        <p>2721 Broadway Ave. Boise, ID</p>
+      </div>
+      <a href="https://www.instagram.com/lovelynailsboise/"><img class="social-logo" alt="instagram-logo" src="images/ig-logo.png"></a>
+      <a href="https://www.facebook.com/lovelynailsboise/?rf=1697763800314152"><img class="social-logo" alt="facebook-logo" src="images/fb-logo.png"></a>
 </nav>
