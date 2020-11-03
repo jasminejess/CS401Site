d81b31ecc14a90a4f7b5219be6ad5f99da38ebc9
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-11-02 22:58:54 -0700

    More forms

--- dao.php
+++ dao.php
@@ -1,13 +1,21 @@
 <?php
 
+require_once 'Klogger.php';
+
 class Dao {
 
     private $db = "lovelynails";
     private $host = "127.0.0.1";
-    protected $user = "jasminejess";
-    protected $pass = "CS401pass";
+    private $user = "jasminejess";
+    private $pass = "CS401pass";
+    private $logger;
+
+    public function __construct () {
+        $this->logger = new KLogger("log.txt", Klogger::DEBUG);
+    }
 
     public function getConnection () {
+        $this->logger->LogDebug("Getting db connection");
         try {
             $conn = new PDO("mysql:host={$this->host};port=8889;dbname={$this->db}", $this->user, $this->pass);
             return $conn;
@@ -18,11 +26,13 @@ class Dao {
     }
 
     public function getUsers () {
+        $this->logger->LogDebug("Getting all users");
         $conn = $this->getConnection();
         return $conn->query("select * from users", PDO::FETCH_ASSOC);
     }
 
     public function addUser ($name, $email, $password) {
+        $this->logger->LogDebug("Trying to add a new user...");
         try {
             $conn = $this->getConnection();
             $query = "Insert into users (email, Name, password) values (:email, :name, :password);";
@@ -31,27 +41,31 @@ class Dao {
             $q->bindParam(":name", $name, PDO::PARAM_STR);
             $q->bindParam(":password", $password);
             $q->execute();
+            $this->logger->LogDebug("New user added successfully");
         } catch (Exception $e) {
-            echo print_r($e, 1);
+            $this->logger->LogDebug($e);
             exit();
         }
     }
 
-    public function userExists($email) {
+    public function userExists ($email) {
+        $this->logger->LogDebug("Trying to find a user...");
         try {
             $conn = $this->getConnection();
-            $query = "Select * from Users where email = :email;";
+            $query = 'Select * from users where email = ":email;"';
             $q = $conn->prepare($query);
             $q->bindParam(":email", $email);
             $q->execute();
             $result = $q->fetchAll(PDO::FETCH_ASSOC);
             if($result > 0) {
+                $this->logger->LogDebug("User found!");
                 return true;
             } else {
+                $this->logger->LogDebug("User doesn't exist");
                 return false;
             }
         } catch (Exception $e) {
-            echo print_r($e, 1);
+            $this->logger->LogDebug($e);
             exit();
         }
     }
