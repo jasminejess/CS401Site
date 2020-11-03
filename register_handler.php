137c7d11459f5ecc30848b877d66e4e8a35aac91
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-11-02 23:04:41 -0700

    More forms

--- register_handler.php
+++ register_handler.php
@@ -1,15 +1,24 @@
 <?php
     session_start();
     require_once 'dao.php';
+    require_once 'Klogger.php';
+    $logger = new Klogger("log.txt", Klogger::DEBUG);
     $_SESSION['output'] = array();
 
     if (strlen($_POST['name']) == 0 || strlen($_POST['email']) == 0 || strlen($_POST['password']) == 0) {
+        $logger->LogInfo("User entered invalid form input");
         $_SESSION['output'] = "Invalid form inputs";
+        header("Location: https://stormy-cliffs-79964.herokuapp.com/register.php");
+        exit();
     }
 
     if (strlen($_POST('password')) < 8 || strlen($_POST) > 64) {
+        $logger->LogInfo("User entered invalid password format");
         $_SESSION['output'] = "Password must be at least 8 characters";
+        header("Location: https://stormy-cliffs-79964.herokuapp.com/register.php");
+        exit();
     }
+    
 
     $dao = new Dao();
 
@@ -21,6 +30,5 @@
         $_SESSION['output'] = "User successfully created";
         header("Location: https://stormy-cliffs-79964.herokuapp.com/login.php");
     }
-    exit();
 
 
