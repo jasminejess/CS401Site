13ce3cfe01e0f15f1244c4bc02244b54778bb1af
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-11-04 10:57:00 -0700

    Forms

--- register_handler.php
+++ register_handler.php
@@ -3,18 +3,18 @@
     require_once 'dao.php';
     require_once 'Klogger.php';
     $logger = new Klogger("log.txt", Klogger::DEBUG);
-    $_SESSION['output'] = array();
+    // $_SESSION['output'] = array();
 
     if (strlen($_POST['name']) == 0 || strlen($_POST['email']) == 0 || strlen($_POST['password']) == 0) {
         $logger->LogDebug("User entered invalid form input");
-        $_SESSION['output'][] = "Invalid form inputs";
+        // $_SESSION['output'][] = "Invalid form inputs";
         header("Location: https://stormy-cliffs-79964.herokuapp.com/register.php");
         exit();
     }
 
     if (strlen($_POST['password']) < 8) {
         $logger->LogDebug("User entered invalid password format");
-        $_SESSION['output'][] = "Password must be at least 8 characters";
+        // $_SESSION['output'][] = "Password must be at least 8 characters";
         header("Location: https://stormy-cliffs-79964.herokuapp.com/register.php");
         exit();
     }
@@ -24,13 +24,13 @@
     $dao = new Dao();
 
     if($dao->userExists($_POST['email'])) {
-        $_SESSION['output'][] = "User with this email already exists";
+        // $_SESSION['output'][] = "User with this email already exists";
         header("Location: https://stormy-cliffs-79964.herokuapp.com/login.php");
         exit();
     }
         
     $dao->addUser($_POST['name'], $_POST['email'], $_POST['password']);
-    $_SESSION['output'][] = "User successfully created";
+    // $_SESSION['output'][] = "User successfully created";
     header("Location: https://stormy-cliffs-79964.herokuapp.com/login.php");
     exit();
     
