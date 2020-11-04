adb766a0a8fdc7f5d55b048c56e90cda3f91c790
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-11-04 10:48:12 -0700

    Forms

--- register.php
+++ register.php
@@ -9,13 +9,13 @@
   <div class="main-page">
     <p class="main-h1">Register as a User</p>
     <div class="main-content">
-        <?php 
+        <!-- <?php 
             if(isset($_SESSION['output'])) {
                 foreach($_SESSION['output'] as $message) {
                     echo "<div class='output{$message}</div>";
                 }
             }
-        ?>
+        ?> -->
         <form method="post" action="register_handler.php">
             <div>Name: <input type="text" name="name"/></div>
             <div>Email: <input type="text" name="Email"/></div>
