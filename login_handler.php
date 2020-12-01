91dcf81a5063e323d5474af9cfa00c53bec866e9
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-11-30 17:38:35 -0700

    edits

--- login_handler.php
+++ login_handler.php
@@ -11,4 +11,8 @@
     if($dao->checkUserLogin($_POST['email'], $_POST['password'])) {
         $_SESSION['output'][] = "Successfully logged in!";
         header("Location: https://stormy-cliffs-79964.herokuapp.com/login.php");
-    } 
+    }  else {
+        $_SESSION['output'][] = "Could not log in";
+        header("Location: https://stormy-cliffs-79964.herokuapp.com/login.php");
+    }
+    exit();
