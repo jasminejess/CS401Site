ad1a568568b8bdec104fbf8f1b3cba8b4f3ff779
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 16:39:33 -0700

    Styling

--- about.php
+++ about.php
@@ -7,6 +7,16 @@
   <div class="main-page">
     <div class="main-content">
       <p class="main-h1">About Us</p>
+      <div class="sub-content">
+        <h1>HOURS</h1>
+        <p>TUESDAY 10AM-7PM</p>
+        <p>WEDNESDAY 10AM-7PM</p>
+        <p>THURSDAY 10AM-7PM</p>
+        <p>FRIDAY 10AM-7PM</p>
+        <p>SATURDAY 10AM-5PM</p>
+        <p>SUNDAY CLOSED</p>
+        <p>MONDAY CLOSED</p>
+      </div>
       <h2>Hours</h2>
     </div>
   </div>
