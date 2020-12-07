1497898a672ce3c5b859c4272791c17197119b40
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 16:19:26 -0700

    Styling

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
 
     <div id="footer">
       <div id="column">
-          <h1>CONTACT</h1>
-          <p>Phone: 208-433-1777</p>
-          <nav>
-            <a href="https://www.instagram.com/lovelynailsboise/"><img class="social-logo" alt="instagram-logo" src="images/ig-logo.png"></a>
-            <a href="https://www.facebook.com/lovelynailsboise/?rf=1697763800314152"><img class="social-logo" alt="facebook-logo" src="images/fb-logo.png"></a>
-          </nav>
-          </div>
-        <div id="column">
-          <h1>ADDRESS</h1>
-          <p>2721 Broadway Ave</p>
-          <p>Boise, ID | 83706</p>
-        </div>
-        <div id="column">
-          <h1>HOURS</h1>
-          <p>Tues-Fri: 10AM-7PM</p>
-          <p>Sat: 10AM-5PM</p>
-        </div>
+        <h1>CONTACT</h1>
+        <p>Phone: 208-433-1777</p>
+        <ul>
+          <li><a href="https://www.instagram.com/lovelynailsboise/"><img class="social-logo" alt="instagram-logo" src="images/ig-logo.png"></a></li>
+          <li><a href="https://www.facebook.com/lovelynailsboise/?rf=1697763800314152"><img class="social-logo" alt="facebook-logo" src="images/fb-logo.png"></a></li>
+        </ul>
+      </div>
+     <div id="column">
+        <h1>ADDRESS</h1>
+        <p>2721 Broadway Ave</p>
+        <p>Boise, ID | 83706</p>
+      </div>
+      <div id="column">
+        <h1>HOURS</h1>
+        <p>Tues-Fri: 10AM-7PM</p>
+        <p>Sat: 10AM-5PM</p>
+      </div>
     </div>
   </body>
 </html>
