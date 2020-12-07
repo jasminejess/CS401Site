ad7934f35d9a2759eae12e49db54815da30d6b70
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 13:28:40 -0700

    Updated with slider

--- index.php
+++ index.php
@@ -6,17 +6,8 @@
   <div class="main-page">
     <p class="main-h1">Welcome to Lovely Nails Salon</p>
     <div class="main-content">
-      <h2>Hours</h2>
       <ul id="lightSlider">
-        <li>
-          <h3>First Slide</h3>
-          <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
-        </li>
-        <li>
-          <h3>Second Slide</h3>
-          <p>Lorem ipsum Excepteur amet adipisicing fugiat velit nisi.</p>
-        </li>
-        <!-- <li data-thumb="gallery_images/IMG_1636.jpg">
+        <li data-thumb="gallery_images/IMG_1636.jpg">
             <img src="gallery_images/IMG_1636.jpg" />
         </li>
         <li data-thumb="gallery_images/FullSizeRender.jpg">
@@ -25,7 +16,7 @@
         <li data-thumb="gallery_images/IMG_0316.jpg">
             <img src="gallery_images/IMG_0316.jpg" />
         </li>
-        <li data-thumb="gallery_images/IMG_0520.jpg">
+        <!-- <li data-thumb="gallery_images/IMG_0520.jpg">
             <img src="gallery_images/IMG_0520.jpg" />
         </li>
         <li data-thumb="gallery_images/IMG_0523.jpg">
