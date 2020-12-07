24bc65b5241d2b7f434c95ff261efbb1e384fa2b
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 16:02:56 -0700

    more formatting

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
-    <hr/>
+
     <div id="footer">
       <div id="column">
-          <h2>CONTACT</h2>
+          <h1>CONTACT</h1>
           <p>Phone: 208-433-1777</p>
         </div>
         <div id="column">
-          <h2>ADDRESS</h2>
+          <h1>ADDRESS</h1>
           <p>2721 Broadway Ave</p>
           <p>Boise, ID | 83706</p>
         </div>
         <div id="column">
-          <h2>HOURS</h2>
+          <h1>HOURS</h1>
           <p>Tues-Fri: 10AM-7PM</p>
           <p>Sat: 10AM-5PM</p>
         </div>
