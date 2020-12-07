fcd56cebf3a4fc9fdb95348c30ce1e5357bccf91
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 13:00:51 -0700

    Updated with slider

--- index.php
+++ index.php
@@ -8,14 +8,24 @@
     <div class="main-content">
       <h2>Hours</h2>
       <ul id="lightSlider">
+        <li data-thumb="gallery_images/IMG_1636.jpg">
+            <img src="gallery_images/IMG_1636.jpg" />
+        </li>
+        <li data-thumb="gallery_images/FullSizeRender.jpg">
+            <img src="gallery_images/FullSizeRender.jpg" />
+        </li>
         <li data-thumb="gallery_images/IMG_0316.jpg">
             <img src="gallery_images/IMG_0316.jpg" />
         </li>
-        <li>
-            <h3>Second Slide</h3>
-            <p>Lorem ipsum Excepteur amet adipisicing fugiat velit nisi.</p>
+        <li data-thumb="gallery_images/IMG_0520.jpg">
+            <img src="gallery_images/IMG_0520.jpg" />
         </li>
-        ...
+        <li data-thumb="gallery_images/IMG_0523.jpg">
+            <img src="gallery_images/IMG_0523.jpg" />
+        </li>
+        <li data-thumb="gallery_images/IMG_1812.jpg">
+            <img src="gallery_images/IMG_1812.jpg" />
+        </li>
       </ul>
     </div>
   </div>
