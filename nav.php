b20e9efd922516abe7e2ab7a05b5e585910528c7
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 16:46:16 -0700

    Styling

--- nav.php
+++ nav.php
@@ -22,6 +22,4 @@
         <p>208-433-1777</p>
         <p>2721 Broadway Ave. Boise, ID</p>
       </div>
-      <a href="https://www.instagram.com/lovelynailsboise/"><img class="social-logo" alt="instagram-logo" src="images/ig-logo.png"></a>
-      <a href="https://www.facebook.com/lovelynailsboise/?rf=1697763800314152"><img class="social-logo" alt="facebook-logo" src="images/fb-logo.png"></a>
     </nav>
