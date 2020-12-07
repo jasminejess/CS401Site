d5834de0ee29e11b859651e3e51eafb969995946
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 15:43:06 -0700

    more formatting

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
     <div id="footer">
       <div id="column">
           <h2>CONTACT</h2>
-          <p>208 433 1777</p>
+          <p>Phone: 208-433-1777</p>
         </div>
         <div id="column">
           <h2>ADDRESS</h2>
-          <p>Lovely Nails Boise ©2020</p>
-          <p>2721 Broadway Ave | Boise, ID | 83706</p>
+          <p>2721 Broadway Ave</p>
+          <p>Boise, ID | 83706</p>
         </div>
         <div id="column">
           <h2>HOURS</h2>
