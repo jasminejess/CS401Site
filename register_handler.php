9451315051303142286b8caf6bc612bf42d4d53c
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 20:02:49 -0700

    Fixed login :)

--- register_handler.php
+++ register_handler.php
@@ -8,6 +8,7 @@
     if (strlen($_POST['name']) == 0 || strlen($_POST['email']) == 0 || strlen($_POST['password']) == 0) {
         $logger->LogDebug("User entered invalid form input");
         $_SESSION['output'][] = "Invalid form inputs";
+        header("Location: https://stormy-cliffs-79964.herokuapp.com/register.php");
         exit();
 
     }
@@ -15,6 +16,7 @@
     if (strlen($_POST['password']) < 8) {
         $logger->LogDebug("User entered invalid password format");
         $_SESSION['output'][] = "Password must be at least 8 characters";
+        header("Location: https://stormy-cliffs-79964.herokuapp.com/register.php");
         exit();
     }
     
