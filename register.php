e400d9e5b5a36340d928510892bee483d16fd0a1
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 18:14:40 -0700

    Styling

--- register.php
+++ register.php
@@ -30,7 +30,7 @@
             <div>
                 <input type="submit" name="register" />
             </div>
-            </hr>
+            <hr/>
             <div>
                 <a href="login.php">Click here to login to an existing account</a>
             </div>
