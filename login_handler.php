b7d7e46b3921b36041924203f13b393358beb772
Jasmine Jess <jasminejess71@u.boisestate.edu>
2020-12-07 18:56:19 -0700

    Styling

--- login_handler.php
+++ login_handler.php
@@ -10,13 +10,13 @@
     $dao = new Dao();
 
     if($dao->checkUserLogin($_POST['email'], $_POST['password']) == true) {
-        $_SESSION["access_granted"] = true;
+        // $_SESSION["access_granted"] = true;
         $_SESSION['output'][] = "Successfully logged in!";
         header("Location: https://stormy-cliffs-79964.herokuapp.com/photos.php");
     }  else {
         $_SESSION['output'][] = "Could not log in";
         $_SESSION["email_preset"] = $_POST["email"];
-        $_SESSION["access_granted"] = false;
+        // $_SESSION["access_granted"] = false;
         header("Location: https://stormy-cliffs-79964.herokuapp.com/login.php");
     }
     exit();
